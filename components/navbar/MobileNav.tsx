476bd4fdfe1e4616c5a3e7a590fcc38bd074ddb1
SouZe-San <2003soumyajit@gamil.com>
2024-07-01 13:38:02 +0530

    NavBar(dev) : Have done navigation bar 🚥

---
+++ components/navbar/MobileNav.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState } from "react";
+import "./menu-style.scss";
+import "./mobile-nav-style.scss";
+import Link from "next/link";
+const MobileNav = () => {
+  const [isMobileNavOpen, setOpenOption] = useState(false);
+
+  return (
+    <div className="md:hidden block">
+      <div className="nav-open-close-btn  md:hidden flex">
+        <div
+          className={`burger ${isMobileNavOpen ? "burger-active" : ""}`}
+          onClick={() => setOpenOption(!isMobileNavOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <nav
+        className={`md:hidden block mobile-nav ${
+          !isMobileNavOpen ? "translate-y-[-100%]" : "translate-y-0"
+        }`}
+      >
+        <ul className="mobile-nav-ul">
+          <li onClick={() => setOpenOption(!isMobileNavOpen)}>
+            <Link href="/">Home</Link>
+          </li>
+          <span className="horizontal-border"></span>
+          <li onClick={() => setOpenOption(!isMobileNavOpen)}>
+            <Link href="/course">Courses</Link>
+          </li>
+          <span className="horizontal-border"></span>
+          <li onClick={() => setOpenOption(!isMobileNavOpen)}>
+            <Link href="/carrier">Carrier</Link>
+          </li>
+          <span className="horizontal-border"></span>
+          <li onClick={() => setOpenOption(!isMobileNavOpen)}>
+            <Link href="/event">Events</Link>
+          </li>
+          <span className="horizontal-border"></span>
+          <li onClick={() => setOpenOption(!isMobileNavOpen)}>
+            <Link href="/contacts">Contact Us</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default MobileNav;
--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import "./nav-style.scss";
+import MobileNav from "./MobileNav";
+
 const NavBar = () => {
   return (
     <header className="nav-header">
       <div className="logo">
         <span>RYATM</span>
       </div>
-      <nav>
+      <nav className="hidden md:flex ">
         <ul>
           <li>
             <Link href="/">Home</Link>
@@ -25,11 +27,12 @@ const NavBar = () => {
           </li>
         </ul>
       </nav>
-      <div className="cta_btns">
+      <div className="cta_btns hidden md:block">
         <button>
           <Link href="/contacts">Connect Us</Link>
         </button>
       </div>
+      <MobileNav />
     </header>
   );
 };
