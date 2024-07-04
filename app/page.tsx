02a569cd3a69bbc49a1de7b017bb5f03e6b3b34a
SouZe-San <2003soumyajit@gamil.com>
2024-07-05 01:52:03 +0530

    some weird circle Rotate 😵‍💫

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import HeroSection from "@/components/home/heroSection/HeroSection";
 import SlideSection from "@/components/home/slideSection/SlideSection";
+import TrendingCourse from "@/components/home/topCourses/TrandingCourses";
+
 export default function Home() {
   return (
     <main className="">
       <HeroSection />
       <SlideSection />
+      <TrendingCourse />
     </main>
   );
 }
