104c76f26f960309a21283945011cee3a072ddbf
SouZe-San <2003soumyajit@gamil.com>
2024-07-04 13:39:06 +0530

    use global-color-variable in css

--- components/course/pageDetails/CoursePage.tsx
+++ components/course/pageDetails/CoursePage.tsx
@@ -1,14 +1,14 @@
-import '@/components/course/pageDetails/Coursepage.scss'
-import { CourseCard } from '../CourseCardSection/CourseCard'
+import "@/components/course/pageDetails/Coursepage.scss";
+import { CourseCard } from "../CourseCardSection/CourseCard";
 
 export const CoursePage = () => {
   return (
     <>
-        <h1 className='font-sans font-normal text-7xl'>Courses</h1>
-        <p>the card is not completed</p>
-        <div id="card-section">
-            <CourseCard/>
-        </div>
+      <h1 className="font-sans font-normal text-7xl">Courses</h1>
+      <p>the card is not completed</p>
+      <div id="card-section">
+        <CourseCard />
+      </div>
     </>
-  )
-}
+  );
+};
