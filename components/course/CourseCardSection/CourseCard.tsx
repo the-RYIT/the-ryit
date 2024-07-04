74ce2bd75959eafeaca9b39e6cd9f3acddb5ea29
lazy34 <maitysurajit0901@gmail.com>
2024-07-04 23:06:20 +0530

    Frontend: Course->Course Card basic design completed

--- components/course/CourseCardSection/CourseCard.tsx
+++ components/course/CourseCardSection/CourseCard.tsx
@@ -1,33 +1,40 @@
 import "@/components/course/CourseCardSection/Course-card.scss";
 import Image from "next/image";
-import dummypic from "@/public/assets/images/course/dummy2jpg.jpg"
 import { ntr } from "@/utils/fonts/fonts";
+import { StaticImageData } from 'next/image';
 
+// making interface for courseCard elements(props)
+interface Prop {
+  Header: string;
+  Description: string;
+  Duration: string;
+  imageSrc: string | StaticImageData;
+}
 
-export const CourseCard = () => {
+export const CourseCard = (props: Prop) => {
+  const { Header, Description, imageSrc, Duration } = props;
   return (
     <>
-      <div id="Card-container">
-        <div id="card-image">
+      <div id="Card-container" className="card-image">
+        <div id="card-image" >
           <Image
-            src={dummypic}
+            src={imageSrc}
             alt="Picture of the Course"
             loading="lazy"
             sizes="(max-width: 768px) 100vw, 33vw"
+            width={500}
+            height={500}
           />
         </div>
         <div id="card-details">
-          <h1 className={`${ntr.className}`}>Python Course</h1>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab vel
-            ipsam ullam, recusandae dolor earum.
-          </p>
+          <h1 className={`${ntr.className}`}>{Header}</h1>
+          <p>{Description}</p>
           <div id="button-div">
             <button id="view-course-btn">View Details</button>
           </div>
           <div id="course-features">
             <h5>Certificate</h5>
-            <h5>3 Months</h5>
+            <h5>{Duration}</h5>
             <h5>Intermidiate</h5>
           </div>
         </div>
