41c5dae0f8dfd102d1d4f0a868578280c4688aea
SouZe-San <2003soumyajit@gamil.com>
2024-07-27 13:01:23 +0530

    NoEventSoon: EventAdd

--- components/events/TopEvents.tsx
+++ components/events/TopEvents.tsx
@@ -1,15 +1,24 @@
-import { ntr } from "@/utils/fonts/fonts";
+import { manrope, ntr } from "@/utils/fonts/fonts";
 import "./styles.scss";
+import EventItem from "./EventItem";
 
 const TopEvents = () => {
   return (
-    <section className="event-top-section flex gap-20">
-      <div className="top-event-section-left-part w-1/4">
+    <section className="event-top-section flex gap-20 sm:flex-row flex-col pr-4">
+      <div className="top-event-section-left-part lg:w-[28%] sm:w-[30%]">
         <h1 className={`event-head-title ${ntr.className}`}> UpComing Events</h1>
         <h5>Your Chances for gain new knowledge is Here - </h5>
       </div>
 
-      <div className="top-event-section-right-part grow flex gap-7"></div>
+      <div className="top-event-section-right-part grow flex max-sm:flex-col gap-7">
+        <h1 className={`event-blank-text sm:text-7xl text-6xl ${manrope.className}`}>
+          Not Any Soon
+        </h1>
+
+        {/* <EventItem id={1} />
+        <EventItem id={2} />
+        <EventItem id={0} /> */}
+      </div>
     </section>
   );
 };
