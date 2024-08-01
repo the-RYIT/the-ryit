bd991894d4e3bdd3c1ebf5c0573c1fc77715c60f
lazy34 <maitysurajit0901@gmail.com>
2024-08-01 13:53:26 +0530

    Frontend : The internship catagory done for desktop

--- components/career/CareerPage.tsx
+++ components/career/CareerPage.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
-import './CareerPage.scss';
-import { ntr } from '@/utils/fonts/fonts';
-import { CatagoryContainer } from './CatagoryContainer';
+import React from "react";
+import "./CareerPage.scss";
+import { ntr } from "@/utils/fonts/fonts";
+import { CatagoryContainer } from "./CatagoryContainer";
 
 const categories = [
-  { title: 'IT', about: 'Explore opportunities in the field of Information Technology.' },
-  { title: 'Finance', about: 'Manage financial strategies and ensure economic growth.' },
-  { title: 'Graphics Design', about: 'Unleash your creativity with innovative designs.' },
-  { title: 'Digital Marketing', about: 'Drive brand engagement through digital channels.' },
-  { title: 'Web Development', about: 'Build and maintain dynamic websites and apps.' },
-  { title: 'Animation', about: 'Bring stories to life with advanced animation techniques.' }
+  {
+    title: "IT",
+    about: "Explore opportunities in the field of Information Technology.",
+    svg: "/assets/icons/career/IT.svg"
+  },
+  {
+    title: "Finance",
+    about: "Manage financial strategies and ensure economic growth.",
+    svg: "/assets/icons/career/finance.svg",
+  },
+  {
+    title: "Graphics Design",
+    about: "Unleash your creativity with innovative designs.",
+    svg: "/assets/icons/career/graphics.svg",
+  },
+  {
+    title: "Digital Marketing",
+    about: "Drive brand engagement through digital channels.",
+    svg: "/assets/icons/career/digital.svg",
+  },
+  {
+    title: "Web Development",
+    about: 'Build and maintain dynamic websites and apps',
+    svg: "/assets/icons/career/web-d.svg",
+  },
+  {
+    title: "Animation",
+    about: "Bring stories to life with advanced animation techniques.",
+    svg: "/assets/icons/career/animation.svg",
+  },
 ];
 
 export const CareerPage = () => {
@@ -22,12 +46,18 @@ export const CareerPage = () => {
       </div>
       <div id="career-subheader">
         <p className={ntr.className}>
-          The RYIT will provide you the opportunity to build a successful career for dynamic youth.
+          The RYIT will provide you the opportunity to build a successful career
+          for dynamic youth.
         </p>
       </div>
       <div id="div-career-catagory-container">
         {categories.map((category, index) => (
-          <CatagoryContainer key={index} title={category.title} about={category.about} />
+          <CatagoryContainer
+            key={index}
+            title={category.title}
+            about={category.about}
+            svg={category.svg}
+          />
         ))}
       </div>
     </>
