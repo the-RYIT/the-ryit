b7e4dfb911b4d0c0e805c6c0fcf4211da578be8f
SouZe-San <2003soumyajit@gamil.com>
2024-08-04 11:13:09 +0530

    EventPage : live data will get

--- components/events/AllEvents.tsx
+++ components/events/AllEvents.tsx
@@ -12,7 +12,7 @@ const AllEvents = async () => {
     projectId,
     dataset,
     apiVersion,
-    useCdn: true,
+    useCdn: false,
   });
   const query = `*[_type == "event" && upcomingEvent != true]{title, slug, eventDate, eventImage{asset->{url}}}`;
   const allEvents: NEventItem.EventItemProps[] = await client.fetch(query);
