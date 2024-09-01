2b89e8c01a756229f83df10607b8c2b88ad3ed50
SouZe-San <2003soumyajit@gamil.com>
2024-09-01 23:50:03 +0530

    Gallery Image Changes

--- components/gallery/TopSection.tsx
+++ components/gallery/TopSection.tsx
@@ -1,16 +1,14 @@
 import Image from "next/image";
 import "./gallery-style.scss";
 import img1 from "@/public/assets/images/gallery/1717090.jpg";
-import img2 from "@/public/assets/images/gallery/20220929_142428.jpg";
-import img3 from "@/public/assets/images/gallery/4b808e46b72570aa62c6a4e79991070c.jpg";
+
 import img4 from "@/public/assets/images/gallery/6a02ce488c6069b5ae898c3da637fa30.jpg";
-import img5 from "@/public/assets/images/gallery/a868d56eb7207e1805ebe475651f7d36.jpg";
-import img6 from "@/public/assets/images/gallery/dd6790167512059.642b260714577.jpg";
+
 import { charm } from "@/utils/fonts/fonts";
 
 const sectionTitle = ["Live Class", "Faculty", "Counselling", "Exhibition", "Seminar"];
 
-const sectionImage = [img6, img3, img2, img4, img5];
+const sectionImage = [img1, img4, img1, img4, img1];
 const TopSection = () => {
   return (
     <section className="p-0 flex flex-nowrap w-screen main-content">
